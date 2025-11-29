caa2d71b0ec971bd71ad127877c416f67e209a16
Denis-Android-Git <124072945+Denis-Android-Git@users.noreply.github.com>
2025-11-29 23:26:06 +0300

    - fixes

--- composeApp/src/commonMain/kotlin/org/example/weathercrossplatform/domain/models/WeatherMainScreenState.kt
+++ composeApp/src/commonMain/kotlin/org/example/weathercrossplatform/domain/models/WeatherMainScreenState.kt
@@ -3,6 +3,18 @@ package org.example.weathercrossplatform.domain.models
 import org.example.weathercrossplatform.data.database.SavedWeatherItem
 import org.example.weathercrossplatform.data.network.dto.ForecastDto
 import org.example.weathercrossplatform.data.utils.UiText
+import org.jetbrains.compose.resources.DrawableResource
+import weathercrossplatform.composeapp.generated.resources.Res
+import weathercrossplatform.composeapp.generated.resources.im_1
+import weathercrossplatform.composeapp.generated.resources.im_10
+import weathercrossplatform.composeapp.generated.resources.im_2
+import weathercrossplatform.composeapp.generated.resources.im_3
+import weathercrossplatform.composeapp.generated.resources.im_4
+import weathercrossplatform.composeapp.generated.resources.im_5
+import weathercrossplatform.composeapp.generated.resources.im_6
+import weathercrossplatform.composeapp.generated.resources.im_7
+import weathercrossplatform.composeapp.generated.resources.im_8
+import weathercrossplatform.composeapp.generated.resources.im_9
 
 data class WeatherMainScreenState(
     val isLoading: Boolean = false,
@@ -14,5 +26,17 @@ data class WeatherMainScreenState(
     val isAddCity: Boolean = false,
     val savedCities: List<SavedWeatherItem> = emptyList(),
     val pageNumberFromSearchScreen: Int? = null,
-    val pageNumber: Int = 0
+    val pageNumber: Int = 0,
+    val appPhotoList: List<DrawableResource> = listOf(
+        Res.drawable.im_1,
+        Res.drawable.im_2,
+        Res.drawable.im_3,
+        Res.drawable.im_4,
+        Res.drawable.im_5,
+        Res.drawable.im_6,
+        Res.drawable.im_7,
+        Res.drawable.im_8,
+        Res.drawable.im_9,
+        Res.drawable.im_10
+    )
 )
