af6730bf6ae5baf7081f80953f3eba63a8c34c32
Denis-Android-Git <124072945+Denis-Android-Git@users.noreply.github.com>
2025-12-16 15:28:04 +0300

    - Ui fixes

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -87,7 +87,7 @@ kotlin {
             implementation(libs.datastore.preferences)
         }
         kotzilla {
-            versionName = "0.9.59" // add your app version name
+            versionName = "0.9.61" // add your app version name
             keyGeneration = KotzillaKeyGeneration.COMPOSE
             composeInstrumentation = true
         }
@@ -109,8 +109,8 @@ android {
         applicationId = "org.example.weathercrossplatform"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 35
-        versionName = "0.9.59"
+        versionCode = 37
+        versionName = "0.9.61"
 
     }
     packaging {
