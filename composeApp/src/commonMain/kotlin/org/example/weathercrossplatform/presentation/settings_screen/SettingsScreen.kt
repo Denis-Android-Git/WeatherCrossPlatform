6f0497598f00faffba9f5cc9740ee89af0940e7e
Denis-Android-Git <124072945+Denis-Android-Git@users.noreply.github.com>
2026-01-30 11:59:35 +0300

    - SearchScreen final

--- composeApp/src/commonMain/kotlin/org/example/weathercrossplatform/presentation/settings_screen/SettingsScreen.kt
+++ composeApp/src/commonMain/kotlin/org/example/weathercrossplatform/presentation/settings_screen/SettingsScreen.kt
@@ -1,17 +1,24 @@
 package org.example.weathercrossplatform.presentation.settings_screen
 
-import androidx.compose.foundation.background
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
@@ -19,8 +26,10 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Alignment
@@ -65,100 +74,148 @@ fun SettingsScreenRoot(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SettingsScreenScreen(
-    modifier: Modifier,
+    modifier: Modifier = Modifier,
     state: SettingsScreenState,
     onAction: (SettingsScreenAction) -> Unit,
     onBackButtonClick: () -> Unit
 ) {
     val backdrop = rememberLayerBackdrop()
     val toggle1 by rememberUpdatedState(newValue = state.isLiquidGlassOn)
-
-    Column(
-        modifier = modifier.fillMaxSize().background(color = Color.Black)
-            .padding(WindowInsets.systemBars.asPaddingValues()).padding(horizontal = 8.dp)
-    ) {
-        IconButton(
-            onClick = onBackButtonClick
-        ) {
-            Icon(
-                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                contentDescription = "back",
-                tint = Color.White
-            )
-        }
-        Spacer(modifier = Modifier.height(32.dp))
-        Text(
-            text = stringResource(Res.string.settings),
-            modifier = Modifier.padding(start = 10.dp),
-            color = Color.White, fontSize = 24.sp
-        )
-        Spacer(modifier = Modifier.height(32.dp))
-        SettingsElement(
-            title = stringResource(Res.string.units)
-        ) {
-            MyDropDownElement(
-                onAction = onAction,
-                state = state,
-                settingsType = SettingsType.TEMPERATURE
-            )
-            Spacer(modifier = Modifier.height(24.dp))
-            MyDropDownElement(
-                onAction = onAction,
-                state = state,
-                settingsType = SettingsType.WIND
-            )
-            Spacer(modifier = Modifier.height(24.dp))
-            MyDropDownElement(
-                onAction = onAction,
-                state = state,
-                settingsType = SettingsType.PRESSURE
-            )
-        }
-        if (state.isLiquidGlassAvailable) {
-            Spacer(modifier = Modifier.height(32.dp))
-            SettingsElement(
-                title = stringResource(Res.string.liquid_glass)
+    val lazyColumnState = rememberLazyListState()
+    val firstVisibleItemIndex by derivedStateOf { lazyColumnState.firstVisibleItemIndex }
+    val animatedAlpha by animateFloatAsState(
+        if (firstVisibleItemIndex > 0) 0f else 1f
+    )
+    Scaffold(
+        modifier = modifier,
+        contentWindowInsets = WindowInsets.safeDrawing,
+        containerColor = Color.Black,
+        topBar = {
+            Box(
+                modifier = Modifier.fillMaxWidth().systemBarsPadding()
             ) {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically
+                IconButton(
+                    onClick = onBackButtonClick,
+                    modifier = Modifier.align(Alignment.CenterStart)
+                ) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "back",
+                        tint = Color.White
+                    )
+                }
+                AnimatedVisibility(
+                    visible = animatedAlpha == 0f,
+                    modifier = Modifier.align(Alignment.Center),
+                    enter = slideInVertically(
+                        initialOffsetY = { it },
+                    ),
+                    exit = slideOutVertically(
+                        targetOffsetY = { it }
+                    )
                 ) {
                     Text(
-                        text = stringResource(Res.string.liquid_glass_desc),
-                        modifier = Modifier.weight(1f),
-                        color = Color.White, fontSize = 16.sp
-                    )
-                    LiquidToggle(
-                        selected = {
-                            toggle1
-                        },
-                        onSelect = {
-                            onAction(SettingsScreenAction.SetLiquidGlass(it))
-                        },
-                        backdrop = backdrop
+                        text = stringResource(Res.string.settings),
+                        color = Color.White, fontSize = 24.sp
                     )
                 }
             }
         }
-        Spacer(modifier = Modifier.height(32.dp))
-        SettingsElement(
-            title = stringResource(Res.string.about_app)
+    ) { paddingValues ->
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.TopCenter
         ) {
-            Row(
-                modifier = Modifier.fillMaxWidth().clickable {
-                    onAction(SettingsScreenAction.OpenPrivacyPolicy)
-                }
+            LazyColumn(
+                modifier = Modifier
+                    .widthIn(max = 600.dp)
+                    .fillMaxSize()
+                    .padding(paddingValues)
+                    .then(
+                        Modifier.padding(horizontal = 8.dp)
+                    ),
+                state = lazyColumnState,
+                contentPadding = PaddingValues(bottom = 8.dp)
             ) {
-                Text(
-                    text = stringResource(Res.string.policy),
-                    modifier = Modifier.weight(1f),
-                    color = Color.White, fontSize = 16.sp
-                )
-                Icon(
-                    imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight,
-                    contentDescription = null,
-                    tint = Color.Gray
-                )
+                item {
+                    Spacer(modifier = Modifier.height(32.dp))
+                    Text(
+                        text = stringResource(Res.string.settings),
+                        modifier = Modifier.padding(start = 10.dp),
+                        color = Color.White, fontSize = 24.sp
+                    )
+                }
+                item {
+                    Spacer(modifier = Modifier.height(32.dp))
+                    SettingsElement(
+                        title = stringResource(Res.string.units)
+                    ) {
+                        MyDropDownElement(
+                            onAction = onAction,
+                            state = state,
+                            settingsType = SettingsType.TEMPERATURE
+                        )
+                        Spacer(modifier = Modifier.height(24.dp))
+                        MyDropDownElement(
+                            onAction = onAction,
+                            state = state,
+                            settingsType = SettingsType.WIND
+                        )
+                        Spacer(modifier = Modifier.height(24.dp))
+                        MyDropDownElement(
+                            onAction = onAction,
+                            state = state,
+                            settingsType = SettingsType.PRESSURE
+                        )
+                    }
+                    if (state.isLiquidGlassAvailable) {
+                        Spacer(modifier = Modifier.height(32.dp))
+                        SettingsElement(
+                            title = stringResource(Res.string.liquid_glass)
+                        ) {
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Text(
+                                    text = stringResource(Res.string.liquid_glass_desc),
+                                    modifier = Modifier.weight(1f),
+                                    color = Color.White, fontSize = 16.sp
+                                )
+                                LiquidToggle(
+                                    selected = {
+                                        toggle1
+                                    },
+                                    onSelect = {
+                                        onAction(SettingsScreenAction.SetLiquidGlass(it))
+                                    },
+                                    backdrop = backdrop
+                                )
+                            }
+                        }
+                    }
+                    Spacer(modifier = Modifier.height(32.dp))
+                    SettingsElement(
+                        title = stringResource(Res.string.about_app)
+                    ) {
+                        Row(
+                            modifier = Modifier.fillMaxWidth().clickable {
+                                onAction(SettingsScreenAction.OpenPrivacyPolicy)
+                            }
+                        ) {
+                            Text(
+                                text = stringResource(Res.string.policy),
+                                modifier = Modifier.weight(1f),
+                                color = Color.White, fontSize = 16.sp
+                            )
+                            Icon(
+                                imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight,
+                                contentDescription = null,
+                                tint = Color.Gray
+                            )
+                        }
+                    }
+                }
             }
         }
     }
